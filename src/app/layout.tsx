fbd940c43438e7c88b3d04fece4cf6b4600e7e89
wptasmina <wptasmina@gmail.com>
2025-06-09 01:31:04 +0600

    page add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PostsPage from "./posts/page";
 
-// import PostsPage from '@/posts/PostsPage';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
