e6e47b0d3d0746568c108e2817b4aca46672771f
wptasmina <wptasmina@gmail.com>
2025-06-09 14:41:05 +0600

    add pnpm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PostsPage from "./posts/page";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PostsPage />
         {children}
       </body>
     </html>
